3cd72b826d724a73eaa701a251b3da4a53e359f1
Vinis-San <112087469+Vinis-San@users.noreply.github.com>
2025-02-27 17:22:00 -0300

    .

---
+++ BackEnd_PHP/livros/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+      rel="stylesheet"
+    />
+    <title>E-Books</title>
+  </head>
+
+  <body>
+    <header>
+      <div class="logo-container">
+        <img src="/imagens/Book Journey2.png" alt="Book Journey Logo" />
+      </div>
+    </header>
+    <main>
+      <div class="Titulo">
+        <h1>Catálogo</h1>
+      </div>
+
+      <section id="catalogo"><div class="container_catalogo">
+                        <div class="description">
+                    <h2 class="tituloproduto">Os Códigos do Milhão</h2>
+                    <h3 class="preco">Inestimável</h3>
+                </div>
+                <div class="botao_container">
+                    <button class="comprarbtn"> Comprar</button>
+                    <button class="Addcarrinho"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart">
+                            <circle cx="9" cy="21" r="1"></circle>
+                            <circle cx="20" cy="21" r="1"></circle>
+                            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                        </svg></button>
+                </div>
+
+
+            </div></section>
+    </main>
+    <footer>
+      
+      <section class="flex-container">
+        <p>Todos os Direitos Reservados.</p>
+        <button class="btnlinkedin">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1.1em"
+            viewBox="0 0 512 512"
+            stroke-width="0"
+            fill="currentColor"
+            stroke="currentColor"
+            class="icon"
+          >
+            <path
+              d="M444.17 32H70.28C49.85 32 32 46.7 32 66.89v374.72C32 461.91 49.85 480 70.28 480h373.78c20.54 0 35.94-18.21 35.94-38.39V66.89C480.12 46.7 464.6 32 444.17 32zm-273.3 373.43h-64.18V205.88h64.18zM141 175.54h-.46c-20.54 0-33.84-15.29-33.84-34.43 0-19.49 13.65-34.42 34.65-34.42s33.85 14.82 34.31 34.42c-.01 19.14-13.31 34.43-34.66 34.43zm264.43 229.89h-64.18V296.32c0-26.14-9.34-44-32.56-44-17.74 0-28.24 12-32.91 23.69-1.75 4.2-2.22 9.92-2.22 15.76v113.66h-64.18V205.88h64.18v27.77c9.34-13.3 23.93-32.44 57.88-32.44 42.13 0 74 27.77 74 87.64z"
+            ></path>
+          </svg>
+          <span class="tooltip">Linkedin</span>
+        </button>
+      </section>
+    </footer>
+    <script src="/aula4_while.js"></script>
+  </body>
+</html>
